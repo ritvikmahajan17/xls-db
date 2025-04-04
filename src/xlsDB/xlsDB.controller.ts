5206706d7ee0fd125d8a1c99747378af6a761d5a
ritvikmahajan17 <56093481+ritvikmahajan17@users.noreply.github.com>
2025-04-05 03:23:39 +0530

    feat: enhance xlsDB controller with detailed API documentation and response handling

--- src/xlsDB/xlsDB.controller.ts
+++ src/xlsDB/xlsDB.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { xlsDBService } from './xlsDB.service';
 import { AddDto } from './dto/add.dto';
 import { FindDto } from './dto/find.dto';
 import { UpdateDto } from './dto/update.dto';
 import { DeleteDto } from './dto/delete.dto';
 import { BatchAddDto } from './dto/batch-add.dto';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { GoogleSheetsService } from './googleSheets.service';
 
 @Controller('xlsDB')
@@ -15,6 +25,21 @@ export class xlsDBController {
     private readonly googleSheetsService: GoogleSheetsService,
   ) {}
 
+  @ApiOperation({ summary: 'Append single data row to Google Sheet' })
+  @ApiResponse({
+    status: 201,
+    description:
+      'Response includes spreadsheetId, updatedRange, updatedRows, updatedColumns, and updatedCells.',
+    schema: {
+      example: {
+        spreadsheetId: '<your-spreadsheet-id>',
+        updatedRange: '<sheet-name>!A4:D4',
+        updatedRows: 1,
+        updatedColumns: 4,
+        updatedCells: 4,
+      },
+    },
+  })
   @Post('add')
   async add(@Body() body: AddDto) {
     const values = body.values;
@@ -37,6 +62,21 @@ export class xlsDBController {
     return response;
   }
 
+  @ApiOperation({ summary: 'Append multiple data rows to Google Sheet' })
+  @ApiResponse({
+    status: 201,
+    description:
+      'Response includes spreadsheetId, updatedRange, updatedRows, updatedColumns, and updatedCells.',
+    schema: {
+      example: {
+        spreadsheetId: '<your-spreadsheet-id>',
+        updatedRange: '<sheet-name>!A4:D4',
+        updatedRows: 1,
+        updatedColumns: 4,
+        updatedCells: 4,
+      },
+    },
+  })
   @Post('batch-add')
   async batchAdd(@Body() body: BatchAddDto) {
     const values = body.values;
@@ -59,8 +99,35 @@ export class xlsDBController {
     return response;
   }
 
+  @ApiOperation({
+    summary: 'Get one data row from Google Sheet, matching the where condition',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        matchingRowIndex: 3,
+        value: {
+          name: 'John Doe',
+          age: '30',
+          city: 'New York',
+        },
+        success: true,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    schema: {
+      example: {
+        matchingRowIndex: -1,
+        value: 'No data found',
+        success: false,
+      },
+    },
+  })
   @Post('get-one')
-  async getOne(@Body() body: FindDto) {
+  async getOne(@Body() body: FindDto, @Res() res: Response) {
     const { where, sheetId, serviceClientEmail, servicePrivateKey, sheetName } =
       body;
 
@@ -69,11 +136,58 @@ export class xlsDBController {
       private_key: servicePrivateKey,
     });
 
-    return await this.xlsDBService.getOne(where, sheetId, sheets, sheetName);
+    const response = await this.xlsDBService.getOne(
+      where,
+      sheetId,
+      sheets,
+      sheetName,
+    );
+
+    if (!response.success) {
+      return res.status(HttpStatus.NOT_FOUND).json(response);
+    }
+    return res.status(HttpStatus.OK).json(response);
   }
 
+  @ApiOperation({
+    summary:
+      'Get all data rows from Google Sheet, matching the where condition',
+    description:
+      'If no where condition is provided, all data rows will be returned.',
+  })
   @Post('get-all')
-  async getAll(@Body() body: FindDto) {
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        matchingRowIndex: [3, 5],
+        value: [
+          {
+            name: 'John Doe',
+            age: '30',
+            city: 'New York',
+          },
+          {
+            name: 'Jane Doe',
+            age: '25',
+            city: 'Los Angeles',
+          },
+        ],
+        success: true,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    schema: {
+      example: {
+        matchingRowIndex: [],
+        value: 'No data found',
+        success: false,
+      },
+    },
+  })
+  async getAll(@Body() body: FindDto, @Res() res: Response) {
     const { where, sheetId, serviceClientEmail, servicePrivateKey, sheetName } =
       body;
 
@@ -82,11 +196,42 @@ export class xlsDBController {
       private_key: servicePrivateKey,
     });
 
-    return await this.xlsDBService.getAll(where, sheetId, sheets, sheetName);
+    const response = await this.xlsDBService.getAll(
+      where,
+      sheetId,
+      sheets,
+      sheetName,
+    );
+
+    if (!response.success) {
+      return res.status(HttpStatus.NOT_FOUND).json(response);
+    }
+    return res.status(HttpStatus.OK).json(response);
   }
 
+  @ApiOperation({
+    summary: 'Update one or more data rows in Google Sheet',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        message: 'Data updated',
+        success: true,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    schema: {
+      example: {
+        message: 'No data found',
+        success: false,
+      },
+    },
+  })
   @Put('update')
-  async update(@Body() body: UpdateDto) {
+  async update(@Body() body: UpdateDto, @Res() res: Response) {
     const {
       where,
       newValues,
@@ -101,17 +246,43 @@ export class xlsDBController {
       private_key: servicePrivateKey,
     });
 
-    return await this.xlsDBService.update(
+    const response = await this.xlsDBService.update(
       where,
       newValues,
       sheetId,
       sheets,
       sheetName,
     );
+
+    if (!response.success) {
+      return res.status(HttpStatus.NOT_FOUND).json(response);
+    }
+    return res.status(HttpStatus.OK).json(response);
   }
 
+  @ApiOperation({
+    summary: 'Delete one or more data rows from Google Sheet',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      example: {
+        message: 'Data deleted',
+        success: true,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    schema: {
+      example: {
+        message: 'No data found',
+        success: false,
+      },
+    },
+  })
   @Delete('delete')
-  async delete(@Body() body: DeleteDto) {
+  async delete(@Body() body: DeleteDto, @Res() res: Response) {
     const { where, sheetId, serviceClientEmail, servicePrivateKey, sheetName } =
       body;
 
@@ -120,7 +291,17 @@ export class xlsDBController {
       private_key: servicePrivateKey,
     });
 
-    return await this.xlsDBService.delete(where, sheetId, sheets, sheetName);
+    const response = await this.xlsDBService.delete(
+      where,
+      sheetId,
+      sheets,
+      sheetName,
+    );
+
+    if (!response.success) {
+      return res.status(HttpStatus.NOT_FOUND).json(response);
+    }
+    return res.status(HttpStatus.OK).json(response);
   }
 
   @ApiExcludeEndpoint()
